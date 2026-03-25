bc1db0160f19530203825c2138fb2ccf1c44dedf
Leo-Carrilho <leocarrilhom@gmail.com>
2026-03-25 20:24:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import LandingPage from './pages/LandingPage';
+import Services from './pages/Services';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
